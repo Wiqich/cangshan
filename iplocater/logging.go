4b51516e44cbd1e364fbcefe90f3952879dd330c
杨晨醒 <chenxing.yang@gmail.com>
2015-09-08 18:38:12 +0800

    sync

--- iplocater/logging.go
+++ iplocater/logging.go
@@ -7,11 +7,21 @@ var Error func(format string, params ...interface{})
 var Fatal func(format string, params ...interface{})
 
 func init() {
-	Debug = defaultDebug
-	Info = defaultInfo
-	Warn = defaultWarn
-	Error = defaultError
-	Fatal = defaultFatal
+	if Debug == nil {
+		Debug = defaultDebug
+	}
+	if Info == nil {
+		Info = defaultInfo
+	}
+	if Warn == nil {
+		Warn = defaultWarn
+	}
+	if Error == nil {
+		Error = defaultError
+	}
+	if Fatal == nil {
+		Fatal = defaultFatal
+	}
 }
 
 func defaultDebug(format string, params ...interface{}) {}
